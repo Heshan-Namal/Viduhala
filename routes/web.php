b34f599617a206c4834016fff69b5b8c56523cbf
taashi619 <lochana.ramanayaka@gmail.com>
2021-10-10 19:17:53 +0530

    check

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 
 //Auth
- 
+ //check
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
